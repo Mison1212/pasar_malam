765b2e6ab6862fad689db2aa17b7e71c0e3fec2c
Mison1212 <misonwenda20@gmail.com>
2026-04-17 10:38:19 +0700

    feat: inisialisasi Firebase dan konfigurasi state management aplikasi
    Menambahkan konfigurasi utama aplikasi pada main.dart termasuk inisialisasi Firebase dan setup Provider.
    
    Fitur:
    - Inisialisasi Firebase menggunakan firebase_options.dart
    - Setup MultiProvider untuk AuthProvider dan ProductProvider
    - Konfigurasi MaterialApp dengan routing menggunakan AppRouter
    - Menentukan initial route ke SplashPage
    - Menonaktifkan debug banner
    
    Tujuan:
    Mempersiapkan struktur utama aplikasi agar mendukung autentikasi Firebase, state management, dan navigasi terpusat.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
+// --- IMPORT ROUTER KAMU ---
+import 'package:pasar_malam/core/routes/app_router.dart';
+import 'package:pasar_malam/features/auth/presentation/providers/auth_provider.dart';
+import 'package:pasar_malam/features/dashboard/presentation/providers/product_provider.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inisialisasi Firebase SEBELUM runApp
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
       child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pasar Malam',
+      initialRoute: AppRouter.splash,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+    );
+  }
 }
