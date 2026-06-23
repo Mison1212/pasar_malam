7df6fa021f080708efc586299064113b2cc60c56
Mison1212 <misonwenda20@gmail.com>
2026-06-24 04:10:36 +0700

    feat: buat kerangka awal komponen AuthGuard
    - Menambahkan `AuthGuard` sebagai *wrapper widget* untuk persiapan implementasi proteksi rute aplikasi.
    - Menginisialisasi struktur dasar yang menerima parameter `child` widget.
    - Catatan: Logika validasi sesi/token pengguna belum diimplementasikan dan saat ini masih mereturn `child` secara langsung (*bypass*). Akan dilanjutkan pada commit berikutnya.

--- lib/core/widgets/auth_guard.dart
+++ lib/core/widgets/auth_guard.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class AuthGuard extends StatelessWidget {
   final Widget child;
 
-  const AuthGuard({Key? key, required this.child}) : super(key: key);
+  const AuthGuard({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add authentication check logic here
-    // For now, just return the child widget
+    
+    
     return child;
   }
 }
